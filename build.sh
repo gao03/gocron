86a52c978f99f7af702cf80bba87ba7e8b4ca516
gaozhiqiang03 <gaozhiqiang03@meituan.com>
2023-02-08 20:09:50 +0800

    build

--- build.sh
+++ build.sh
@@ -1,11 +1,1 @@
-git pull --rebase
-git tag -d v1.6.0
-git tag v1.6.0
-
-make package
-mv *-package/*.tar.gz ~/bin/gocron
-cd ~/bin/gocron
-tar -zxvf *.tar.gz
-mv *-amd64/* ./
-rm -rf *-amd64*
-
+git pull --rebase && git tag -d v1.6.0 ; git tag v1.6.0 && make package && mv *-package/*.tar.gz ~/bin/gocron && cd ~/bin/gocron && x *.tar.gz && mv *-amd64/* ./ && rm -rf *-amd64* && ./gocron-node
